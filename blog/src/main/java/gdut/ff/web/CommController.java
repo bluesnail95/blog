b5dadd82f065a26f6a09835b99a7fe4d262a8864
bluesnail95 <13642315483@163.com>
2018-02-10 19:24:10 +0800

    添加获取项目根路径的接口

---
+++ blog/src/main/java/gdut/ff/web/CommController.java
@@ -0,0 +1,37 @@
+package gdut.ff.web;
+
+import java.io.FileNotFoundException;
+
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import gdut.ff.utils.NodeUtil;
+
+/**
+ * 
+ * @author liuffei
+ * @date 2018-02-10
+ */
+@RestController
+public class CommController {
+
+	/**
+	 * 获取项目根路径
+	 * @return
+	 */
+	@GetMapping(value="/comm/path")
+	public ObjectNode rootPath() {
+		ObjectNode result = NodeUtil.create();
+		try {
+			String root = ResourceUtils.getURL("classpath:").getPath();
+			result.put("root", root);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		result.put("status", 1);
+		return result;
+	}
+}
