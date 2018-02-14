3497547f01eaa95ea9feef58876e39f96aeeebee
bluesnail95 <13642315483@163.com>
2018-02-14 17:27:17 +0800

    完成个人中心头像上传，目前还没加上token验证，文件保存在target/class/upload下，每次启动之前上传的会丢失。

--- blog/src/main/resources/templates/js/user/person.js
+++ blog/src/main/resources/templates/js/user/person.js
@@ -22,4 +22,39 @@ function module(name){
 
 window.onload = function(){
 	init();
+
+    /**
+     *上传头像
+     */
+	new Vue({
+        el: "form",
+        data: {
+            file: '',
+            url: '../../image/user/profile.gif'
+        },
+        methods: {
+            uploadImg(event){
+                this.file = event.target.files[0];
+                console.log(this.file);
+            },
+            submitForm(event){
+                event.preventDefault();
+                let formData = new FormData();
+                formData.append("file",this.file);
+
+                let config = {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                };
+
+                axios.post("http://127.0.0.1:8080/upload/user", formData, config)
+                    .then(function(response){
+                        if(response.status == 200){
+                            console.log("success"); 
+                        }
+                    })
+            }
+        }
+    })
 }
