922bd89657d5144a65198880dbac2de73c01f0dd
bluesnail95 <13642315483@163.com>
2018-03-30 00:28:56 +0800

    添加markdown编辑器，用于发布自己写的博客

--- blog/src/main/java/gdut/ff/web/BlogController.java
+++ blog/src/main/java/gdut/ff/web/BlogController.java
@@ -1,41 +1,75 @@
 package gdut.ff.web;
 
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import gdut.ff.utils.BlogUtil;
-import gdut.ff.utils.HttpUtil;
+import gdut.ff.domain.Blog;
+import gdut.ff.service.BlogServiceImpl;
 import gdut.ff.utils.NodeUtil;
 
 /**
- * 用于获取主页的数据
+ * 用于获取我的博客的数据
  * @author liuffei
  * @date 2018-01-25
  */
 @RestController
 @RequestMapping(value="/blog")
 public class BlogController {
+	
+	@Autowired
+	private BlogServiceImpl blogServiceImpl;
     
 	/**
-	 * 加载主页轮播图的数据(来自csdn或是自己写的博客)
+	 * 查询全部的用户
 	 * @return
 	 */
-	@RequestMapping(value = "/csdn/list",method = RequestMethod.GET)
-	public ObjectNode csdnBlog(HttpServletResponse resp) {
+	@PostMapping(value = "/blog")
+	public ObjectNode insertBlog(@RequestBody Blog blog) {
 		ObjectNode result = NodeUtil.create();
-		//现在是写死的的链接，后期要修改成前端传入的参数
-		String html = HttpUtil.httpRequest("https://www.csdn.net/");
-		ArrayNode blogs = new BlogUtil().filterHtml(html);
-		ArrayNode content = result.putArray("blogs");
-		content.addAll(blogs);
-		result.put("status",1);
-		//resp.setHeader("Access-Control-Allow-Origin","*");
+		blogServiceImpl.insertBlog(blog);
+		result.put("status", 1);
+		return result;
+	}
+	
+	@GetMapping(value = "/blog/{id}")
+	public ObjectNode findBlogById(@PathVariable String id) {
+		ObjectNode result = NodeUtil.create();
+		Blog blog = blogServiceImpl.fingOneById(id);
+		result.putPOJO("content", blog);
+		result.put("status", 1);
+		return result;
+	}
+	
+	@PutMapping(value = "/blog/{id}")
+	public ObjectNode updateBlogById(@PathVariable String id) {
+		ObjectNode result = NodeUtil.create();
+		
+		result.put("status", 1);
+		return result;
+	}
+	
+	@DeleteMapping(value = "/blog/{id}")
+	public ObjectNode deleteBlogById(@PathVariable String id) {
+		ObjectNode result = NodeUtil.create();
+		
+		result.put("status", 1);
+		return result;
+	}
+	
+	@GetMapping(value = "/blogs")
+	public ObjectNode finaAllBlogs() {
+		ObjectNode result = NodeUtil.create();
+		
+		result.put("status", 1);
 		return result;
 	}
 }
