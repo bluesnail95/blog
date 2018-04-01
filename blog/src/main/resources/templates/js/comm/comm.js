878959a61bf38902f0e19dda0d5804f18285cc85
bluesnail95 <13642315483@163.com>
2018-04-01 16:00:15 +0800

    新的前端页面，使用amazeui.

--- blog/src/main/resources/templates/js/comm/comm.js
+++ blog/src/main/resources/templates/js/comm/comm.js
@@ -18,58 +18,51 @@
 /**
  *path 从后台获取项目路径
  */
-var path,router;
-axios.get('http://127.0.0.1:8081/comm/path')
-    .then(function(response){
-        path = response.data.root;
+var router;
 
-        if(initRouter(path)){
-        	/**
-        	 *定义标题导航组件
-        	 */
-        	Vue.component('my-nav',{
-			    //props:['index_href','contact_href','about_href','csdn_href','cnblogs_href','osc_href','login_href'],
-			    template:'<ul class="header">'
-					      +   '<li><a v-bind:href="index_href" class="active">主页</a></li>'
-					      +   '<div class="dropdown">'
-						  +       '<a href="#" class="dropbtn">博客网站</a>'
-						  +       '<div class="dropdown-content">'
-						  +	          '<a v-bind:href="csdn_href">CSDN</a>'
-						  +	          '<a v-bind:href="cnblogs_href">博客园</a>'
-						  +	          '<a v-bind:href="osc_href">开源中国</a>'
-						  +        '</div>'
-					      +   '</div>'
-					      +   '<li><a v-bind:href="analysis_href">数据分析</a></li>'
-					      +   '<li><a v-bind:href="contact_href">联系我</a></li>'
-					      +   '<li><a v-bind:href="about_href">关于</a></li>'
-					      +   '<li style="float: right;"><a v-bind:href="login_href" v-if="login_seen">登录</a></li>'
-					      +   '<div class="dropdown" v-if="person_seen" style="float: right;margin-right:100px;">'
-					      +       '<img v-bind:src="profile" style="width:50px;height:50px;" class="profile-login"/>'
-						  +       '<div class="dropdown-content">'
-						  +	          '<a v-bind:href="person_href">个人主页</a>'
-						  +	          '<a onclick="logout();">退出</a>'
-						  +        '</div>'
-					      +   '</div>'
-				          +'</ul>',
-				data:function(){
-					return router;
-				}
-			})
 
-            /**
-             *定义组件的作用范围
-             */
-			new Vue({
-				el:"#top"
-			})
-        }      
+if(initRouter()){
+	/**
+	 *定义标题导航组件
+	 */
+	Vue.component('my-nav',{
+	    //props:['index_href','contact_href','about_href','csdn_href','cnblogs_href','osc_href','login_href'],
+	    template:'<ul class="header">'
+			      +   '<li><a v-bind:href="index_href" class="active">主页</a></li>'
+			      +   '<div class="dropdown">'
+				  +       '<a href="#" class="dropbtn">博客网站</a>'
+				  +       '<div class="dropdown-content">'
+				  +	          '<a v-bind:href="csdn_href">CSDN</a>'
+				  +	          '<a v-bind:href="cnblogs_href">博客园</a>'
+				  +	          '<a v-bind:href="osc_href">开源中国</a>'
+				  +        '</div>'
+			      +   '</div>'
+			      +   '<li><a v-bind:href="analysis_href">数据分析</a></li>'
+			      +   '<li><a v-bind:href="contact_href">联系我</a></li>'
+			      +   '<li><a v-bind:href="about_href">关于</a></li>'
+			      +   '<li style="float: right;"><a v-bind:href="login_href" v-if="login_seen">登录</a></li>'
+			      +   '<div class="dropdown" v-if="person_seen" style="float: right;margin-right:100px;">'
+			      +       '<img v-bind:src="profile" style="width:50px;height:50px;" class="profile-login"/>'
+				  +       '<div class="dropdown-content">'
+				  +	          '<a v-bind:href="person_href">个人主页</a>'
+				  +	          '<a onclick="logout();">退出</a>'
+				  +        '</div>'
+			      +   '</div>'
+		          +'</ul>',
+		data:function(){
+			return router;
+		}
+	})
 
-    })
-    .catch(function(error){
+    /**
+     *定义组件的作用范围
+     */
+	new Vue({
+		el:"#top"
+	})
+}      
 
-    });
-
-function initRouter(path){
+function initRouter(){
     router = {
 	    'index_href':"/html/index.html",
 	    'contact_href':"/html/contact.html",
---
+++ view/assets/js/comm.js
@@ -0,0 +1,6 @@
+//定义全局的服务器路径
+let SERVER_URL = "http://127.0.0.1:8081";
+
+let $ = function(id){
+  return document.getElementById(id);
+}
