7f677015fabb0ac6373175b72665a68eb4f09f65
bluesnail95 <13642315483@163.com>
2018-04-14 22:33:16 +0800

    写新博客时清空sessionStorage的博客数据

---
+++ view/assets/js/admin-comm.js
@@ -0,0 +1,17 @@
+new Vue({
+	el: "#admin-menu",
+	data: function() {
+		  return {
+          blogs: []
+      };
+	},
+	mounted: function() {
+      
+  },
+  methods: {
+    	write() {
+  			  sessionStorage.removeItem("blogId");
+  			  window.location.href = "../../admin/admin-edit.html";
+  	  }
+  }
+})
